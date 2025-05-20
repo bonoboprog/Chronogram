b416608654ff7171163fa0b283aefdaaa60f3107
viole <violeta.perezdiaz@studentmail.unicas.it>
2025-05-20 12:19:35 +0200

    class

--- src/it/unicas/activities/dao/LoginDAO.java
+++ src/it/unicas/activities/dao/LoginDAO.java
@@ -1,4 +1,43 @@
 package it.unicas.activities.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.mindrot.jbcrypt.BCrypt; // We can't put it for now
+
+import it.unicas.activities.dbutil.DBUtil;
+import it.unicas.activities.pojo.LoginInfo;
+
 public class LoginDAO {
+
+    public static boolean isUserValid(LoginInfo userDetails) {
+        boolean validStatus = false;
+
+        try {
+            Connection conn = DBUtil.getConnection();
+
+            String sql = "SELECT password_hash FROM user_auth WHERE email = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, userDetails.getUserName());
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                String storedHash = rs.getString("password_hash");
+
+                // Verifica la password usando BCrypt
+                if (BCrypt.checkpw(userDetails.getPassword(), storedHash)) {
+                    validStatus = true;
+                }
+            }
+
+            DBUtil.closeConnection(conn);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return validStatus;
+    }
 }
--- src/it/unicas/activities/dbutil/DBUtil.java
+++ src/it/unicas/activities/dbutil/DBUtil.java
@@ -1,4 +1,31 @@
 package it.unicas.activities.dbutil;
 
+import java.sql.*;
+
 public class DBUtil {
+
+    public static Connection getConnection()
+    {
+        Connection conn = null;
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/chronogram_admin?useSSL=false&serverTimezone=UTC", "chronogram_admin", "diary_user1234");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
+    public static void closeConnection(Connection conn)
+    {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
