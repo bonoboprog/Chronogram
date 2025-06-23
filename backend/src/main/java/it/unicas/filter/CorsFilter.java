848ccfeeae31856fdfff2d7a189c870d2e895738
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2025-06-23 15:56:35 +0200

    Update CorsFilter.java

--- backend/src/main/java/it/unicas/filter/CorsFilter.java
+++ backend/src/main/java/it/unicas/filter/CorsFilter.java
@@ -14,6 +14,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true"); // for cookie or Authorization
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         response.setHeader("Access-Control-Max-Age", "86400");
